ea1c055088103d17e486683523b0354f580a1132
rorr73 <27837292+rorr73@users.noreply.github.com>
2018-06-10 17:35:04 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lifesospy',
-    version='0.4.1',
+    version='0.4.2',
     packages=['lifesospy'],
     install_requires=['aenum'],
     python_requires='>=3.5.3',
